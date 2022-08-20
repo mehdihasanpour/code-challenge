86238dd773594b890f1a06ed47ad26dd95f635e9
mehdi hasanpour <me.hasani1367@gmail.com>
2022-08-20 22:34:40 +0430

    style changes

--- LargestPrimeFactor.php
+++ LargestPrimeFactor.php
@@ -1,14 +1,14 @@
 <?php
 
-function isFactor(int $number,int $factor): bool
+function isFactor(int $number, int $factor): bool
 {
-    return ($number%$factor==0);
+    return ($number % $factor == 0);
 }
 
 function isPrime(int $num): bool
 {
-    for($i=2;$i<$num;$i++){
-        if($num%($i)==0){
+    for ($i = 2; $i < $num; $i++) {
+        if ($num % ($i) == 0) {
             return false;
         }
     }
