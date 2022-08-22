3211d8b16fed74b70e37c34722463daa535f18ba
mehdi hasanpour <me.hasani1367@gmail.com>
2022-08-23 00:52:46 +0430

    some refactor

--- AppleOrange.php
+++ AppleOrange.php
@@ -2,11 +2,11 @@
 
 function countApplesAndOranges($s, $t, $a, $b, $apples, $oranges)
 {
-    $apples = array_filter($apples,function($apple) use ($a, $t, $s){
+    $apples = array_filter($apples, function ($apple) use ($a, $t, $s) {
         $distance = $a + $apple;
         return ($distance <= $t && $distance >= $s);
     });
-    $oranges = array_filter($oranges,function($orange) use ($b, $t, $s){
+    $oranges = array_filter($oranges, function ($orange) use ($b, $t, $s) {
         $distance = $b + $orange;
         return ($distance <= $t && $distance >= $s);
     });
@@ -17,4 +17,4 @@ function countApplesAndOranges($s, $t, $a, $b, $apples, $oranges)
 
 
 countApplesAndOranges(2, 3, 1, 5, [2], [-2]);
-countApplesAndOranges(7, 11, 5, 15, [-2,2,1], [5, -6]);
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
