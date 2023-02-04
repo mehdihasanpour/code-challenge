fa5c9c1f9327559be858903b7b88f8da4827b5f6
mehdi hasanpour <me.hasani1367@gmail.com>
2023-02-05 00:14:03 +0330

    style fixing

--- isAnagram.php
+++ isAnagram.php
@@ -2,23 +2,24 @@
 
 declare(strict_types=1);
 
-class Solution {
+class Solution
+{
 
     function isAnagram(string $s, string $t): bool
     {
-        if(strlen($s) != strlen($t)){
+        if (strlen($s) != strlen($t)) {
             return false;
         }
 
         $charMapForS = [];
         $charMapForT = [];
-        for($i=0; $i<strlen($s); $i++){
-            $charMapForS[$s[$i]] ++;
-            $charMapForT[$t[$i]] ++;
+        for ($i = 0; $i < strlen($s); $i++) {
+            $charMapForS[$s[$i]]++;
+            $charMapForT[$t[$i]]++;
         }
 
-        foreach($charMapForS as $key => $item){
-            if($charMapForT[$key] != $item){
+        foreach ($charMapForS as $key => $item) {
+            if ($charMapForT[$key] != $item) {
                 return false;
             }
         }
@@ -28,7 +29,7 @@ class Solution {
 
 $solution = new Solution;
 
-$result1 = $solution->isAnagram('car','tar');
-$result2 = $solution->isAnagram('anagram','naaagrm');
+$result1 = $solution->isAnagram('car', 'tar');
+$result2 = $solution->isAnagram('anagram', 'naaagrm');
 
-var_dump($result1,$result2);
+var_dump($result1, $result2);
