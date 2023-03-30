f7532913da566c83f888c5cbebfae19e94253cec
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-03-31 00:59:25 +0330

    write binary search in go language

---
+++ BinarySearch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func binarySearch2(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	target := 5
+
+	fmt.Println(binarySearch2(nums, target))
+}
